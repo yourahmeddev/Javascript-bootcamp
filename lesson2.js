88f0a5f0b2fb2deb55357c644e980cbf42460163
yourahmeddev <ahmedhassansheikh7@gmail.com>
2023-02-23 17:07:37 +0500

    Variable and datatype in js

---
+++ lesson2.js
@@ -0,0 +1,10 @@
+var eng = 53;
+// eng = 45;
+eng = 53 + 10;
+console.log(eng);
+let  maths = 67;
+maths = 67 - 10;
+console.log(maths);
+const pf = 98;
+// pf = 100;  //we cannot assign new value into the constant variable
+console.log(pf);
